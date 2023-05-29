17ce288100fa0057c3489486b58a213cc785f550
ClaudiaRojasSoto <claudiarojassoto@gmail.com>
2023-05-29 15:41:03 -0400

    Add const to save documnts catched with getElementBy Id in script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let bookList = document.getElementById('book-list');
+let inputTitle = document.getElementById('title');
+let inputAuthor = document.getElementById('author');
+let inputButton = document.getElementById('add-button');
+
+let books =[];
+
+function addBooks (event){
+ event.preventDefault();
+}
