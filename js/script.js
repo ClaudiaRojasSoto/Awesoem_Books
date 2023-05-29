7abc16b8b7e831e4daa02b61503fc22c432bd4e1
HFG43 <117187653+HFG43@users.noreply.github.com>
2023-05-29 14:59:47 -0600

    Add Remove Button Id Selection

--- js/script.js
+++ js/script.js
@@ -31,15 +31,15 @@ function displayBooks(){
       listItemAuthor.textContent = books[i].author;
       bookList.appendChild(listItemAuthor);
       const removeButoon = document.createElement('button');
+      removeButoon.id = [i];
       removeButoon.textContent = 'remove';
-      removeButoon.setAttribute('data-index', i);
-      
+      // removeButoon.setAttribute('data-index', i);
       bookList.appendChild(removeButoon);     
    }
 }
 displayBooks();
 
-function removeBook(index) {
+function removeBook(event) {
    books.splice(index, 1);
   
    displayBooks();
