cacd44283a16cbb3007e76f75e27ff371f5b369b
HFG43 <117187653+HFG43@users.noreply.github.com>
2023-05-29 14:50:32 -0600

    Add display Book Function

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ let bookList = document.getElementById('book-list');
 let inputTitle = document.getElementById('title');
 let inputAuthor = document.getElementById('author');
 let inputButton = document.getElementById('add-button');
+let removeButoon = document.getElementById('remove-Button');
 
-let books =[];
+let books = JSON.parse(localStorage.getItem('book')) || [];
+
 
 function addBooks (event){
 
@@ -15,6 +17,31 @@ function addBooks (event){
     books.unshift(book);
     localStorage.setItem('book', JSON.stringify(books));
  }
+ displayBooks();
 }
-
 inputButton.addEventListener('click', addBooks);
+
+function displayBooks(){
+   bookList.innerHTML = '';
+   for(let i = 0; i <books.length; i += 1){
+      const listItemTitle = document.createElement('p');
+      listItemTitle.textContent = books[i].title;
+      bookList.appendChild(listItemTitle);
+      const listItemAuthor = document.createElement('p');
+      listItemAuthor.textContent = books[i].author;
+      bookList.appendChild(listItemAuthor);
+      const removeButoon = document.createElement('button');
+      removeButoon.textContent = 'remove';
+      removeButoon.setAttribute('data-index', i);
+      
+      bookList.appendChild(removeButoon);     
+   }
+}
+displayBooks();
+
+function removeBook(index) {
+   books.splice(index, 1);
+  
+   displayBooks();
+ }
+ removeButoon.addEventListener('click', removeBook);
