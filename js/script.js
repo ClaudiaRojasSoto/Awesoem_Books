342e848c39a4f5fea4649f04b2c79c5da21862b2
ClaudiaRojasSoto <claudiarojassoto@gmail.com>
2023-05-29 15:58:51 -0400

    create the addBooks function to add books and save it in local storage as an object on script.js

--- js/script.js
+++ js/script.js
@@ -6,5 +6,15 @@ let inputButton = document.getElementById('add-button');
 let books =[];
 
 function addBooks (event){
+
  event.preventDefault();
+ const title = inputTitle.value;
+ const author = inputAuthor.value;
+ if (title !== '' && author !== ''){
+    const book = {title, author};
+    books.unshift(book);
+    localStorage.setItem('book', JSON.stringify(books));
+ }
 }
+
+inputButton.addEventListener('click', addBooks);
