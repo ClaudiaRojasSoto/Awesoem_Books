c7380a3973c7192d7d93b39eb73f9c1b04b1e3ff
HFG43 <117187653+HFG43@users.noreply.github.com>
2023-05-30 09:46:48 -0600

    Add functions inside Classes

--- js/script.js
+++ js/script.js
@@ -1,57 +1,68 @@
-const bookList = document.getElementById('book-list');
-const inputTitle = document.getElementById('title');
-const inputAuthor = document.getElementById('author');
-const inputButton = document.getElementById('add-button');
-
-let books = JSON.parse(localStorage.getItem('book')) || [];
-
-function addBooks(event) {
-  event.preventDefault();
-  const title = inputTitle.value;
-  const author = inputAuthor.value;
-  if (title !== '' && author !== '') {
-    const book = { id: Date.now(), title, author };
-    books = [book, ...books];
-    localStorage.setItem('book', JSON.stringify(books));
+class Books {
+  constructor(author,title, id) {
+    this.author = author;
+    this.title = title;
+    this.id = id;
+    this.bookList = document.getElementById('book-list');
+    this.inputTitle = document.getElementById('title');
+    this.inputAuthor = document.getElementById('author');
+    this.inputButton = document.getElementById('add-button');
+    this.inputButton.addEventListener('click', this.addBooks);    
+    this.books = JSON.parse(localStorage.getItem('book')) || []; 
   }
-  displayBooks();
-  inputAuthor.value = '';
-  inputTitle.value = '';
-}
-inputButton.addEventListener('click', addBooks);
-
-function displayBooks() {
-  bookList.innerHTML = '';
-  for (let i = 0; i < books.length; i += 1) {
-    const listItemTitle = document.createElement('p');
-    listItemTitle.textContent = books[i].title;
-    bookList.appendChild(listItemTitle);
-    const listItemAuthor = document.createElement('p');
-    listItemAuthor.textContent = books[i].author;
-    bookList.appendChild(listItemAuthor);
-    const removeButoon = document.createElement('button');
-    removeButton.id = `remove-button-${books[i].id}`;
-    removeButoon.textContent = 'remove';
-    removeButoon.setAttribute('data-index', i);
-    bookList.appendChild(removeButoon);
-    const line = document.createElement('hr');
-    bookList.appendChild(line);
+  addBooks(event) {
+    event.preventDefault();
+    const title = this.inputTitle.value;
+    const author = this.inputAuthor.value;
+    if (title !== '' && author !== '') {
+      const book = { id: Date.now(), title, author };
+      this.books = [book, ...books];
+      localStorage.setItem('book', JSON.stringify(books));
+    }
+    this.displayBooks();
+    this.inputAuthor.value = '';
+    this.inputTitle.value = '';
+  }
+  
+  displayBooks() {
+    bookList.innerHTML = '';
+    for (let i = 0; i < this.books.length; i += 1) {
+      const listItemTitle = document.createElement('p');
+      listItemTitle.textContent = this.books[i].title;
+      this.bookList.appendChild(listItemTitle);
+      const listItemAuthor = document.createElement('p');
+      listItemAuthor.textContent = this.books[i].author;
+      this.bookList.appendChild(listItemAuthor);
+      const removeButoon = document.createElement('button');
+      removeButoon.id = `remove-button-${this.books[i].id}`;
+      removeButoon.textContent = 'remove';
+      removeButoon.setAttribute('data-index', i);
+      this.bookList.appendChild(removeButoon);
+      const line = document.createElement('hr');
+      this.bookList.appendChild(line);
+    }  
+    this.attachRemoveButtonListeners();
   }
 
-  attachRemoveButtonListeners();
-}
-displayBooks();
+    attachRemoveButtonListeners() {
+      const removeButtons = this.document.querySelectorAll("button[id^='remove-button-']");
+      removeButtons.forEach((button) => {
+      button.addEventListener('click', this.removeBook);
+    });
+  }
 
-function attachRemoveButtonListeners() {
-  const removeButtons = document.querySelectorAll("button[id^='remove-button-']");
-  removeButtons.forEach((button) => {
-    button.addEventListener('click', removeBook);
-  });
-}
-
-function removeBook(event) {
-  const bookIndex = event.target.dataset.index;
-  books.splice(bookIndex, 1);
+removeBook(event) {
+  const bookIndex = this.event.target.dataset.index;
+  this.books.splice(bookIndex, 1);
   localStorage.setItem('book', JSON.stringify(books));
-  displayBooks();
+  this.displayBooks();
+ }
 }
+
+
+
+
+
+
+
+
