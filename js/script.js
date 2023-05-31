bd93113bb6bf7bc414dbf0a8e64fe0b64117a54a
HENOK MEKONNEN <Henokme@ethiopianairlines.com>
2023-05-31 20:31:56 +0300

    added date and fix on show content

--- js/script.js
+++ js/script.js
@@ -1,102 +1,119 @@
-import Book from './book.js';
+import Book from "./book.js";
 
 class BookList {
-  constructor() {
-    this.bookList = document.getElementById('book-list');
-    this.inputTitle = document.getElementById('title');
-    this.inputAuthor = document.getElementById('author');
-    this.inputButton = document.getElementById('add-button');
-    this.listButton = document.getElementById('contact-section');
-    this.newButton = document.getElementById('form-section');
-    this.books = JSON.parse(localStorage.getItem('book')) || [];
-    this.inputButton.addEventListener('click', (event) => this.addBooks(event));
-    this.displayBooks();
+    constructor() {
+        this.bookList = document.getElementById("book-list");
+        this.inputTitle = document.getElementById("title");
+        this.inputAuthor = document.getElementById("author");
+        this.inputButton = document.getElementById("add-button");
+        this.listButton = document.getElementById("contact-section");
+        this.newButton = document.getElementById("form-section");
+        this.books = JSON.parse(localStorage.getItem("book")) || [];
+        this.inputButton.addEventListener("click", (event) => this.addBooks(event));
+        this.displayBooks();
 
-    this.listLink = document.getElementById('list');
-    this.addLink = document.getElementById('new');
-    this.contactLink = document.getElementById('contact');
+        this.listLink = document.getElementById("list");
+        this.addLink = document.getElementById("new");
+        this.contactLink = document.getElementById("contact");
 
-    this.listLink.addEventListener('click', ()=> this.showSection('div-list'));
-    this.addLink.addEventListener('click', ()=> this.showSection('form-section'));
-    this.contactLink.addEventListener('click', ()=> this.showSection('contact-section'));
+        this.listLink.addEventListener("click", () => this.showSection("div-list"));
+        this.addLink.addEventListener("click", () => this.showSection("form-section"));
+        this.contactLink.addEventListener("click", () => this.showSection("contact-section"));
 
-  }
-
-  showSection('div-list')
-  addBooks(event) {
-    event.preventDefault();
-    const title = this.inputTitle.value;
-    const author = this.inputAuthor.value;
-    if (title !== '' && author !== '') {
-      const book = new Book(Date.now(), title, author);
-      this.books = [book, ...this.books];
-      localStorage.setItem('book', JSON.stringify(this.books));
-    }
-    this.displayBooks();
-    this.inputAuthor.value = '';
-    this.inputTitle.value = '';
-  }
-
-  showSection (sectionId) {
-   // event.preventDefault();
-    //this.bookList.classList.add('hidden');
-   // this.newButton.classList.add('hidden');
-    //this.listButton.classList.add('hidden');
-    let sections = document.getElementsByClassName('list-section');
-    for(let i = 0; i <sections.length ; i ++){
-      let content = sections[i]
-      if(content.id === sectionId){
-        content.classList.remove('hidden');
-      }else {
-        content.classList.add('active');
-      }
+        this.displayDate();
     }
 
-    //const sectionToShow = document.getElementById(sectionId);
-    //sectionToShow.classList.remove('hidden');
-    //sectionToShow.classList.add('active')
-  }
-
-  displayBooks() {
-    this.bookList.innerHTML = '';
-    const list = document.createElement('ul');
-    list.classList.add('list-container');
-
-    for (let i = 0; i < this.books.length; i += 1) {
-      const book = this.books[i];
-
-      const listItem = document.createElement('li');
-
-      const booksInfo = document.createElement('span');
-      booksInfo.textContent = `${book.title} by ${book.author}`;
-      listItem.classList.add(i % 2 === 0 ? 'even' : 'odd');
-      listItem.appendChild(booksInfo);
-      const removeButoon = document.createElement('button');
-      removeButoon.id = `remove-button-${this.books[i].id}`;
-
-      removeButoon.textContent = 'remove';
-      removeButoon.setAttribute('data-index', i);
-
-      listItem.appendChild(removeButoon);
-      list.appendChild(listItem);
+    addBooks(event) {
+        event.preventDefault();
+        const title = this.inputTitle.value;
+        const author = this.inputAuthor.value;
+        if (title !== "" && author !== "") {
+            const book = new Book(Date.now(), title, author);
+            this.books = [book, ...this.books];
+            localStorage.setItem("book", JSON.stringify(this.books));
+        }
+        this.displayBooks();
+        this.inputAuthor.value = "";
+        this.inputTitle.value = "";
     }
-    this.bookList.appendChild(list);
-    this.attachRemoveButtonListeners();
-  }
 
-  attachRemoveButtonListeners() {
-    const removeButtons = document.querySelectorAll("button[id^='remove-button-']");
-    removeButtons.forEach((button) => {
-      button.addEventListener('click', this.removeBook.bind(this));
-    });
-  }
+    showSection(sectionId) {
+        // event.preventDefault();
+        // this.bookList.classList.add("hidden");
+        // this.newButton.classList.add("hidden");
+        // this.listButton.classList.add("active");
+        let sections = document.getElementsByClassName("list-section");
 
-  removeBook(event) {
-    const bookIndex = event.target.dataset.index;
-    this.books.splice(bookIndex, 1);
-    localStorage.setItem('book', JSON.stringify(this.books));
-    this.displayBooks();
-  }
+        for (let i = 0; i < sections.length; i++) {
+            let content = sections[i];
+            if (content.id === sectionId) {
+                content.classList.remove("hidden");
+            } else {
+                content.classList.add("hidden");
+            }
+        }
+
+        //const sectionToShow = document.getElementById(sectionId);
+        //sectionToShow.classList.remove('hidden');
+        //sectionToShow.classList.add('active')
+    }
+
+    displayDate() {
+        const dateElement = document.getElementById("date");
+        let today = new Date();
+        const options = {
+            month: "short",
+            day: "numeric",
+            year: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+            second: "numeric"
+        };
+        const format = today.toLocaleTimeString("en-US", options);
+
+        dateElement.textContent = format;
+    }
+
+    displayBooks() {
+        this.bookList.innerHTML = "";
+        const list = document.createElement("ul");
+        list.classList.add("list-container");
+
+        for (let i = 0; i < this.books.length; i += 1) {
+            const book = this.books[i];
+
+            const listItem = document.createElement("li");
+
+            const booksInfo = document.createElement("span");
+            booksInfo.textContent = `${book.title} by ${book.author}`;
+            listItem.classList.add(i % 2 === 0 ? "even" : "odd");
+            listItem.appendChild(booksInfo);
+            const removeButoon = document.createElement("button");
+            removeButoon.id = `remove-button-${this.books[i].id}`;
+
+            removeButoon.textContent = "remove";
+            removeButoon.setAttribute("data-index", i);
+
+            listItem.appendChild(removeButoon);
+            list.appendChild(listItem);
+        }
+        this.bookList.appendChild(list);
+        this.attachRemoveButtonListeners();
+    }
+
+    attachRemoveButtonListeners() {
+        const removeButtons = document.querySelectorAll("button[id^='remove-button-']");
+        removeButtons.forEach((button) => {
+            button.addEventListener("click", this.removeBook.bind(this));
+        });
+    }
+
+    removeBook(event) {
+        const bookIndex = event.target.dataset.index;
+        this.books.splice(bookIndex, 1);
+        localStorage.setItem("book", JSON.stringify(this.books));
+        this.displayBooks();
+    }
 }
 
 const bookList = new BookList();
